788c8510376933107d7392416c44da9647b30b36
Mārtiņš Spektors <martins-spektors@users.noreply.github.com>
2025-11-10 11:29:48 +0200

    MVC: Rework index controller to return an HTML view.

---
+++ src/main/kotlin/rest/controllers/IndexController.kt
@@ -0,0 +1,25 @@
+package lv.ailab.senie.rest.controllers
+
+import lv.ailab.senie.db.entities.Book
+import lv.ailab.senie.db.repositories.BookRepository
+import org.springframework.stereotype.Controller
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.ModelAttribute
+import java.util.*
+
+@Controller
+class IndexController(
+    private val bookRepo: BookRepository,
+) {
+    @GetMapping("", "/", "/index")
+    fun listBooks(): String = "index"
+
+    @ModelAttribute("booksByCentury")
+    fun booksByCentury(): SortedMap<Int, List<Book>> {
+        return bookRepo.findAll()
+            .filter { it.orderInCollection == 0 && it.itemCode != null }
+            .groupBy { it.pubCentury }
+            .toSortedMap()
+    }
+
+}
