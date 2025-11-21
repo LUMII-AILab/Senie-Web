fb3d97f0d6631f7b3feb8069a9405e65a6e928e1
Mārtiņš Spektors <martins.spektors@gmail.com>
2025-11-21 16:56:49 +0200

    Goto: Implement go-to address.

--- src/main/kotlin/rest/controllers/GotoController.kt
+++ src/main/kotlin/rest/controllers/GotoController.kt
@@ -3,14 +3,14 @@ package lv.ailab.senie.rest.controllers
 import lv.ailab.senie.db.repositories.BookRepository
 import lv.ailab.senie.db.repositories.ContentRepository
 import lv.ailab.senie.rest.CommonFailures.bookNotFound
-import lv.ailab.senie.utils.urlEncode
 import org.springframework.stereotype.Controller
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.servlet.view.RedirectView
+import org.springframework.web.util.UriComponentsBuilder
+import java.util.*
 
-@Controller
-class GotoController(
+@Controller class GotoController(
     private val bookRepo: BookRepository,
     private val contentRepo: ContentRepository,
 ) {
@@ -18,20 +18,28 @@ class GotoController(
     fun goto(
         @RequestParam("source") source: String?,
         @RequestParam("address") address: String?,
-        @RequestParam("page") page: String?,
+        @RequestParam("page") inputPage: String?,
     ): RedirectView {
-        if (address != null) {
-            error("Vēl nav uztaisīts.")
-        } else if (source != null) {
-            val target =
-                bookRepo.findByFullSource(source)
-                    ?.urlPath?.let { path ->
-                        if (page != null)
-                            "$path?page=${page.urlEncode()}"
-                        else path
-                    }
-                    ?: throw bookNotFound(source)
-            return RedirectView(target)
-        } else throw Exception("`/goto` adresei nepieciešams `address` vai `source` parametrs.")
+        val (book, page, line) = when {
+            address != null -> {
+                val line = contentRepo.findByAddress(address)
+                    ?: error("Sistēmā nav rindiņas ar adresi [$address].")
+                val book = bookRepo.findByFullSource(line.source)!!
+                Triple(book, line.page, line)
+            }
+            source != null -> {
+                val book = bookRepo.findByFullSource(source) ?: throw bookNotFound(source)
+                Triple(book, inputPage, null)
+            }
+            else -> throw Exception("`/goto` adresei nepieciešams `address` vai `source` parametrs.")
+        }
+
+        val target = UriComponentsBuilder
+            .fromPath(book.urlPath)
+            .queryParamIfPresent("page", Optional.ofNullable(page))
+            .let { if (line != null) it.fragment(line.address) else it }
+            .build()
+            .toUriString()
+        return RedirectView(target)
     }
 }
