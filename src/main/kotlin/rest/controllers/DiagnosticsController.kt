acf32c7db12f8629aaae1681b20c351c2e6e626e
Mārtiņš Spektors <232586491+martins-spektors@users.noreply.github.com>
2026-01-29 16:06:40 +0200

    controller

---
+++ src/main/kotlin/rest/controllers/DiagnosticsController.kt
@@ -0,0 +1,14 @@
+package lv.ailab.senie.rest.controllers
+
+import org.springframework.stereotype.Controller
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestMapping
+
+@Controller
+@RequestMapping("diagnostics")
+class DiagnosticsController {
+    @GetMapping
+    fun index(): String {
+        return "diagnostics"
+    }
+}
