7a4bfd2ab9d96e3f51e4dcdba763014496453b3d
Mārtiņš Spektors <232586491+martins-spektors@users.noreply.github.com>
2026-01-29 15:09:04 +0200

    Authors: Implement a more idiomatic fetching of author names through a many-to-many intermediate table.

---
+++ src/main/kotlin/db/entities/Author.kt
@@ -0,0 +1,12 @@
+package lv.ailab.senie.db.entities
+
+import jakarta.persistence.Entity
+import jakarta.persistence.Id
+import jakarta.persistence.Table
+
+@Entity
+@Table(name = "authors")
+data class Author(
+    @Id val id: Int,
+    val name: String,
+)
