dd12f60a6f4699b99ff74f43840edcba01a3b0d4
Mārtiņš Spektors <martins.spektors@gmail.com>
2026-01-29 15:39:49 +0200

    Pages: Update page entity to match current DB structure.

--- src/main/kotlin/db/entities/Page.kt
+++ src/main/kotlin/db/entities/Page.kt
@@ -3,17 +3,21 @@ package lv.ailab.senie.db.entities
 import jakarta.persistence.Entity
 import jakarta.persistence.Id
 import jakarta.persistence.Table
+import java.time.Instant
 
 @Entity
 @Table(name = "pages")
 data class Page(
     @Id val id: Int,
+    val source: String,
     val name: String?,
-    val order: Int,
+    val sortOrder: Int,
+    val facsimileFilename: String?,
+    val facsimileCheckedOn: Instant?,
 ) {
 
     val linkName: String?
-        get() = if (order == 0 && name == null) "_" else name
+        get() = if (sortOrder == 0 && name == null) "_" else name
 
     val displayName: String?
         get() = if (linkName == "_") "Titullapa" else name
