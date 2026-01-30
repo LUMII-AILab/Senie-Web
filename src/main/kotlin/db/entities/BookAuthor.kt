29fc74addd9a5deeef60a5108d4f1add8302997f
Mārtiņš Spektors <232586491+martins-spektors@users.noreply.github.com>
2026-01-30 15:20:07 +0200

    DB: Implement a composite primary key for the book-author join entity.

--- src/main/kotlin/db/entities/BookAuthor.kt
+++ src/main/kotlin/db/entities/BookAuthor.kt
@@ -6,17 +6,24 @@ import jakarta.persistence.FetchType.LAZY
 @Entity
 @Table(name = "books_authors")
 data class BookAuthor(
-    @Id val id: Int,
+    @EmbeddedId val id: CompositeId,
 
+    @MapsId("source")
     @ManyToOne(fetch = LAZY)
     @JoinColumn(name = "source", referencedColumnName = "fullSource")
     val book: Book,
 
+    @MapsId("authorId")
     @ManyToOne(fetch = LAZY)
-    @JoinColumn(name = "authorId")
+    @JoinColumn(name = "author_id")
     val author: Author,
 
     /* This is a property describing author's relation to a book, not author itself. */
     @Column("cover_author") // TODO: Rename column to match
     val isCoverAuthor: Boolean,
-)
+) {
+    class CompositeId {
+        var source: String? = null
+        var authorId: Int? = null
+    }
+}
